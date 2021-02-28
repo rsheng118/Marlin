4edf0cec3a6528b0a510d5a941e2ae93a94e8740
Robert Sheng <wokao118@gmail.com>
2021-02-28 13:48:49 -0600

    chore: update steps per mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 640, 640, 1600, 5268}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 640, 640, 1600, 5450}
 
 /**
  * Default Max Feed Rate (mm/s)
